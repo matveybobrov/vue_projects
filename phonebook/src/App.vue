85bba351eaf7374a13c6cc584cce6a01e38abe05
Matvey <matveybobrov01@gmail.com>
2023-11-15 12:29:35 +0300

    2.6: The Phonebook Step1

---
+++ phonebook/src/App.vue
@@ -0,0 +1,40 @@
+<script>
+export default {
+  data() {
+    return {
+      newName: '',
+      people: [{
+        name: 'Arto Hellas'
+      }]
+    }
+  },
+  methods: {
+    addPerson() {
+      const newPerson = {
+        name: this.newName
+      }
+      this.people.push(newPerson)
+      this.newName = ''
+    }
+  }
+}
+</script>
+
+<template>
+   <div>
+      <h2>Phonebook</h2>
+      <form @submit.prevent="addPerson">
+        <div>
+          name: <input v-model="newName"/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div v-for="person in people" :key="person.name">{{ person.name }}</div>
+    </div>
+</template>
+
+<style scoped>
+</style>
