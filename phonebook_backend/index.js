b09c5dec1674b7c73287fddb7af14f9f11ae04a8
Matvey <matveybobrov01@gmail.com>
2023-11-15 15:52:10 +0300

    3.3: Phonebook backend step3

--- phonebook_backend/index.js
+++ phonebook_backend/index.js
@@ -24,15 +24,27 @@ const people = [
   },
 ]
 
-app.get('/api/people', (req, res) => {
-  res.json(people)
-})
-
 app.get('/info', (req, res) => {
   res.send(`
     <div>Phonebook has info for ${people.length} people</div>
     <div>${new Date().toLocaleString()}</div>
   `)
+})
+
+app.get('/api/people', (req, res) => {
+  res.json(people)
+})
+
+app.get('/api/people/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const person = people.find((person) => person.id === id)
+
+  if (!person) {
+    return res.status(404).json({
+      error: 'No person with such id',
+    })
+  }
+  res.json(person)
 })
 
 const PORT = 3001
