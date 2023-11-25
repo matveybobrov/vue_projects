827b5126714839c6b372461590451a06aa8e7a09
matveybobrov <matveybobrov01@gmail.com>
2023-11-25 17:17:21 +0300

    fix: return 404 on empty blog

--- bloglist_backend/controllers/blogs.js
+++ bloglist_backend/controllers/blogs.js
@@ -8,9 +8,16 @@ blogsRouter.get('/', async (req, res) => {
   res.json(blogs)
 })
 
+blogsRouter.get('/:id', async (req, res) => {
+  const blog = await Blog.findById(req.params.id)
+  if (!blog) {
+    return res.status(404).end()
+  }
+  res.json(blog)
+})
+
 blogsRouter.post('/', async (req, res) => {
   const blog = new Blog(req.body)
-
   const savedBlog = await blog.save()
   res.status(201).json(savedBlog)
 })
@@ -18,6 +25,14 @@ blogsRouter.post('/', async (req, res) => {
 blogsRouter.delete('/:id', async (req, res) => {
   await Blog.findByIdAndDelete(req.params.id)
   res.status(204).end()
+})
+
+blogsRouter.put('/:id', async (req, res) => {
+  const updatedBlog = await Blog.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  })
+  res.json(updatedBlog)
 })
 
 export default blogsRouter
